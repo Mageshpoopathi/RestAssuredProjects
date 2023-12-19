3134153409e8ee13022e0956df2fbb3d5af02c3f
Mageshpoopathi <mageshpoopathi208@gmail.com>
2023-12-19 08:06:17 +0530

    testNG framework

---
+++ Rest/src/main/java/com/rest/endPoints/Routes.java
@@ -0,0 +1,12 @@
+package com.rest.endPoints;
+
+public class Routes {
+    public static String BASE_URL="https://petstore.swagger.io/v2";
+
+    public static String post_url=BASE_URL+"/user";
+    public static String get_url=BASE_URL+"/user/{username}";
+    public static String update_url=BASE_URL+"/user/{username}";
+    public static String delete_url=BASE_URL+"/user/{username}";
+
+
+}
